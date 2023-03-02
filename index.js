d7dbf181a5192d1c5838951ea9876d4d03658a21
Gaurav sarma <gauravsarma68@gmail.com>
2023-03-02 22:05:00 +0530

    index.js file was added

---
+++ index.js
@@ -0,0 +1,14 @@
+function add(a){
+    console.log(13/2);
+    a();
+    return function(){
+        console.log("something");
+    }
+}
+var res=add(function(a,b){
+    console.log(a+b);
+})
+
+var resfinal=res();
+console.log()
+
